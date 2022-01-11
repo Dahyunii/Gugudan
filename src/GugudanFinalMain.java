022269cd6db4ea05450151efafa6290d140d1b83
Dahyun <Dahyun@localhost>
2022-01-12 00:03:02 +0900

    GugudanFinal 구

---
+++ src/GugudanFinalMain.java
@@ -0,0 +1,12 @@
+
+public class GugudanFinalMain {
+
+	public static void main(String[] args) {
+		for(int i = 2; i < 10; i++) {
+			int[] result = GugudanClass.calculate(i);
+			GugudanClass.print(result);
+		}
+
+	}
+
+}
